be2c974e996116ea83c07dfe0f718fc668c144fa
Misha Huziuk <mishahuziuk@Mishas-MacBook-Air.local>
2024-05-31 16:39:09 -0500

    install nodemon

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import initRoutes from "./routes";
 
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+initRoutes(app);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
