b463d30d852d1f897b8ba6564440deeb87c8bf1f
Misha Huziuk <mihagu36@gmail.com>
2024-05-31 22:44:23 -0500

    add swagger jsdoc

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import swaggerUI from "swagger-ui-express";
 import initRoutes from "./routes";
+import swaggerSpec from "./swagger";
 
 const app: Express = express();
 const port = 3000;
 
 initRoutes(app);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
--- src/api/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Express, Request, Response } from "express";
-
-const initRoutes = (app: Express) => {
-    app.get("/", (req: Request, res: Response) => {
-        res.send("Express + TypeScript Server");
-    });
-}
-
-export default initRoutes;
