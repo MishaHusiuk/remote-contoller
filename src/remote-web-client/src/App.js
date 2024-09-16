874429d33102d7d0fb24f7c18347504e87db5125
Misha Huziuk <mihagu36@gmail.com>
2024-09-16 19:25:06 -0400

    `mobile-web-client` initial commit

---
+++ src/remote-web-client/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-dvh">
+      <div className="p-8 bg-gray-600 flex flex-col justify-between h-full">
+        <div className="grid grid-cols-3 grid-rows-3 gap-4">
+          <div className="p-4 bg-teal-500 rounded-md flex items-center justify-center row-start-1 col-start-1">
+            <button onClick={() => alert('esc')}>esc</button>
+          </div>
+          <div className="p-4 bg-teal-500 rounded-md flex items-center justify-center row-start-1 col-start-2 col-span-2">
+            <button onClick={() => alert('enter')}>enter</button>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 grid-rows-4 gap-4">
+          <div className="p-4 bg-yellow-400 rounded-md flex items-center justify-center row-start-1 col-start-2">
+            <button onClick={() => alert('up!')}>↑</button>
+          </div>
+          <div className="p-4 bg-yellow-400 rounded-md flex items-center justify-center row-start-2 col-start-1">
+            <button onClick={() => alert('left!')}>←</button>
+          </div>
+          <div className="p-4 bg-yellow-400 rounded-md flex items-center justify-center row-start-2 col-start-3">
+            <button onClick={() => alert('right!')}>→</button>
+          </div>
+          <div className="p-4 bg-yellow-400 rounded-md flex items-center justify-center row-start-2 col-start-2">
+            <button onClick={() => alert('down!')}>↓</button>
+          </div>
+          <div className="p-4 bg-teal-500 rounded-md flex items-center justify-center row-start-4 col-start-1 col-span-3">
+            <button onClick={() => alert('space')}>space</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
