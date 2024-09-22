7b76aa0c57825b4503f9bac6736429e9fc0dd575
Misha Huziuk <mihagu36@gmail.com>
2024-09-21 21:41:03 -0400

    hide the app in the tray by default

--- desktop-client/main.js
+++ desktop-client/main.js
@@ -1,17 +1,25 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 const { initWebSocket } = require('./ws-connection');
 
+let isQuiting;
+let tray;
+let window;
+
 const createWindow = () => {
+    // const icon = nativeImage.createFromPath(path.resolve(__dirname, 'images/main-icon/remote.png'));
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
-        }
+        },
+        show: false,
+        // icon
     })
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+    window = win;
     return win;
 };
 
@@ -26,8 +34,37 @@ app.whenReady().then(() => {
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) initApp();
     })
+
+    tray = new Tray(path.join(__dirname, 'images', 'RemoteTemplate@2x.png'));
+
+    tray.setContextMenu(Menu.buildFromTemplate([
+        {
+            label: 'Show app',
+            click: () => window.show()
+        },
+        {
+            label: 'Quit',
+            click: () => {
+                isQuiting = true;
+                app.quit();
+            }
+        }
+    ]));
+
+    window.on('close', (event) => {
+        if (!isQuiting) {
+            event.preventDefault();
+            window.hide();
+            event.returnValue = false;
+        }
+    });
 })
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
+
+app.on('before-quit', () => {
+    isQuiting = true;
+});
