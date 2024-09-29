a4f38fa0868c4e0e7f44b1556534828fbb0698b6
Misha Huziuk <mihagu36@gmail.com>
2024-09-28 22:13:25 -0400

    change express routing to always return `index.html` file when non `api` route requested, to support client-side routing

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,10 +9,15 @@ const app: Express = express();
 const port = 3001;
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../../remote-web-client/build')));
 
 app.use('/api', router);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+app.use(express.static(path.join(__dirname, '../../remote-web-client/build')));
+// Route all client-side routes to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../remote-web-client/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
