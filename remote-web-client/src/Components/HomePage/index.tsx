6b85b348054cbff0e0fbb27db4a1c8072de76350
Misha Huziuk <mihagu36@gmail.com>
2024-10-17 21:22:13 -0400

    add windows download url

--- remote-web-client/src/Components/HomePage/index.tsx
+++ remote-web-client/src/Components/HomePage/index.tsx
@@ -1,7 +1,19 @@
 import Footer from '../Footer';
 import Profile from '../Profile';
 
+const url = "https://remote-control-assets.s3.amazonaws.com/remote-control/win32/x64/%D0%B4%D0%B8%D1%81%D1%82%D0%B0%D0%BD%D1%86%D1%96%D0%B9%D0%BD%D0%B8%D0%B9-%D0%B7%D0%B2%D1%8F%D0%B7%D0%BE%D0%BA.exe"
 function HomePage() {
+  const handleDownload = () => {
+    const fileUrl = url;
+    const fileName = '%D0%B4%D0%B8%D1%81%D1%82%D0%B0%D0%BD%D1%86%D1%96%D0%B9%D0%BD%D0%B8%D0%B9-%D0%B7%D0%B2%D1%8F%D0%B7%D0%BE%D0%BA.exe';  // The name to save the file as
+
+    const anchor = document.createElement('a');
+    anchor.href = fileUrl;
+    anchor.download = fileName;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+};
   return (
       <div className="flex-1 flex flex-col items-center justify-center p-8">
         <section className="text-center mb-12">
@@ -13,11 +25,10 @@ function HomePage() {
         <section className="flex flex-col items-center space-y-8 mb-12">
           <div className="flex space-x-8">
             {/* MacOS Download */}
-            <div className="flex flex-col items-center">
+            {/* <div className="flex flex-col items-center">
               <button className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-6 rounded-lg">
                 Завантажити для MacOS
               </button>
-              {/* MacOS Chip Architecture Selection */}
               <div className="mt-2">
                 <label className="block text-sm mb-1">Виберіть архітектуру процесору:</label>
                 <select className="bg-gray-800 border border-gray-600 text-white py-1 px-2 rounded-lg">
@@ -25,11 +36,11 @@ function HomePage() {
                   <option value="arm">Apple Silicon (M1/M2)</option>
                 </select>
               </div>
-            </div>
+            </div> */}
 
             {/* Windows Download */}
             <div className="flex flex-col items-center">
-              <button className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-6 rounded-lg">
+              <button className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-6 rounded-lg" onClick={handleDownload}>
                 Завантажити для Windows
               </button>
             </div>
